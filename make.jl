6ddca5dfd25baec31f763756abf88048ab75376f
Max Horn <max@quendi.de>
2025-10-21 15:22:37 +0200

    Merge update.jl into make.jl

--- make.jl
+++ make.jl
@@ -1,5 +1,6 @@
 using Documenter
 using Markdown
+using JSON
 
 # Types and functions to generate a Markdown table with links to package badges etc.
 struct PackageDefinition
@@ -39,6 +40,58 @@ function package_table_markdown(packages)
         push!(table.rows, markdown(p))
     end
     Markdown.MD(table)
+end
+
+function generate_package_table_markdown()
+    # query information about all JuliaDocs repositories
+    data = read(`gh repo list JuliaDocs --json "isArchived,description,homepageUrl,name,url,defaultBranchRef"`, String)
+    repos = JSON.parse(data)
+
+    # exclude archived repositories
+    repos = filter!(x -> !x["isArchived"], repos)
+
+    # exclude some other repositories
+    excluded = [
+        "DocumentationGeneratorRegistry",   # kinda obsolete
+        "DocumenterInventoryWritingBackport.jl",
+        "juliadocs.github.io",              # this repository, no point linking to it
+        "Julia-Cheat-Sheet",                # not a package
+    ]
+    filter!(x -> x["name"] ∉ excluded, repos)
+
+    # sort alphabetical
+    sort!(repos, by = x -> x["name"])
+
+    packages = PackageDefinition[]
+    for r in repos
+        name = r["name"]
+        name_no_suffix = endswith(name, ".jl") ? name[1:(end - 3)] : name
+        url = r["url"]
+        homepageUrl = r["homepageUrl"]
+        desc = r["description"]
+
+        docs = Pair{String, String}[]
+        if isempty(homepageUrl)
+            push!(docs, "README" => "https://github.com/JuliaDocs/$name")
+        else
+            # HACK / workaround for Highlights.jl
+            if endswith(homepageUrl, "/stable")
+                homepageUrl = replace(homepageUrl, "/stable" => "")
+            end
+            push!(docs, "stable" => joinpath(homepageUrl, "stable/"))
+            push!(docs, "dev" => joinpath(homepageUrl, "dev/"))
+        end
+
+        branch = r["defaultBranchRef"]["name"]
+        buildbadges = Pair{String, String}[
+            "https://codecov.io/gh/JuliaDocs/$name/branch/$branch/graph/badge.svg" =>
+                "https://codecov.io/gh/JuliaDocs/$name",
+        ]
+
+        push!(packages, PackageDefinition(name_no_suffix, url, desc, docs, buildbadges))
+    end
+
+    return package_table_markdown(packages)
 end
 
 # Build the docs
